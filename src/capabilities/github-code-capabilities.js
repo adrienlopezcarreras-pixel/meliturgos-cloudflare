438a46f85585da377a5ddb642bba57fff7005b63
adrienlopezcarreras-pixel <adrien.lopezcarreras@gmail.com>
2026-09-10 07:26:21 +0200

    release(r3): align code reader fallback

--- src/capabilities/github-code-capabilities.js
+++ src/capabilities/github-code-capabilities.js
@@ -1,4 +1,4 @@
-const DEFAULT_BRANCH = 'release/mel-2026-09-09-r2-2';
+const DEFAULT_BRANCH = 'release/mel-2026-09-10-r3';
 const MAX_FILE_BYTES = 180_000;
 const MAX_SEARCH_FILES = 24;
 const MAX_MATCHES = 20;
