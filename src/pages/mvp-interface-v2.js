fef2d03d7363502269b4566217c2a4b75d487b4d
adrienlopezcarreras-pixel <adrien.lopezcarreras@gmail.com>
2026-09-12 10:32:26 +0200

    ui: remove redundant normal-mode behavior layer

--- src/pages/mvp-interface-v2.js
+++ src/pages/mvp-interface-v2.js
@@ -1,11 +1,5 @@
 import { onRequestGet as renderMvp } from './mvp-interface.js';
-import { enhanceMvpBehavior } from './mvp-behavior-enhancer.js';
 
 export async function onRequestGet(context){
-  let response=await renderMvp(context);
-  response=await enhanceMvpBehavior(response);
-  const headers=new Headers(response.headers);
-  headers.delete('content-length');
-  headers.set('cache-control','no-store, max-age=0');
-  return new Response(await response.text(),{status:response.status,statusText:response.statusText,headers});
+  return renderMvp(context);
 }
