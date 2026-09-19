52d02fbef161f759eacf0b34071188d3e91d6047
adrienlopezcarreras-pixel <adrien.lopezcarreras@gmail.com>
2026-09-19 15:31:46 +0200

    fix(device): align MEL UI types with LVGL 8

--- firmware/waveshare-terminal/main/mel_terminal.h
+++ firmware/waveshare-terminal/main/mel_terminal.h
@@ -6,6 +6,6 @@
 #define MEL_LCD_V_RES 480
 #define MEL_FW_VERSION "0.1.0"
 
-void mel_terminal_ui_init(lv_display_t *display);
+void mel_terminal_ui_init(lv_disp_t *display);
 void mel_terminal_start(bool force_setup);
 void mel_terminal_set_hardware(bool camera_ok, bool audio_ok, bool sd_ok);
