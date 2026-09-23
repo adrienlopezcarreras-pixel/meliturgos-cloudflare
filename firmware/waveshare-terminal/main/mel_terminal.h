78e49732ec32bd3a2f28296566dc40bf2bb04a00
adrienlopezcarreras-pixel <adrien.lopezcarreras@gmail.com>
2026-09-23 21:32:50 +0200

    MINI: expose external UI binding for runtime text

--- firmware/waveshare-terminal/main/mel_terminal.h
+++ firmware/waveshare-terminal/main/mel_terminal.h
@@ -27,3 +27,5 @@ enum MelTerminalState {
 void mel_terminal_request_voice(void);
 int mel_terminal_state(void);
 bool mel_terminal_online(void);
+
+void mel_terminal_bind_external_ui(lv_obj_t *status_label, lv_obj_t *answer_label);
