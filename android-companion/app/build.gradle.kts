3e3c11365912a38f91e0d22aa95e2486cc32645e
adrienlopezcarreras-pixel <adrien.lopezcarreras@gmail.com>
2026-09-23 15:41:24 +0200

    build(android): bump diagnostic share build to 0.6.1

--- android-companion/app/build.gradle.kts
+++ android-companion/app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "fr.veriteinterdite.mel"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "0.6.0"
+        versionCode = 10
+        versionName = "0.6.1"
         buildConfigField("String", "MEL_BASE_URL", "\"https://meliturgos.adrien-lopezcarreras.workers.dev\"")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
