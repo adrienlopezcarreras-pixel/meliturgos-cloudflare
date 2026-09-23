a53767719901b9631e9d5ceeed8ca3f60eca8141
adrienlopezcarreras-pixel <adrien.lopezcarreras@gmail.com>
2026-09-23 18:45:12 +0200

    MINI: bump physical companion test firmware version

--- firmware/waveshare-terminal/main/mel_terminal.h
+++ firmware/waveshare-terminal/main/mel_terminal.h
@@ -4,7 +4,7 @@
 
 #define MEL_LCD_H_RES 320
 #define MEL_LCD_V_RES 480
-#define MEL_FW_VERSION "0.1.0"
+#define MEL_FW_VERSION "0.2.0-mini-test"
 #define MEL_PROTOCOL_VERSION "1.0"
 
 void mel_terminal_ui_init(lv_disp_t *display);
