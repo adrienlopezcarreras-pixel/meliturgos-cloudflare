b5489f10671ad9e33a10691f01883e6971b19ade
adrienlopezcarreras-pixel <adrien.lopezcarreras@gmail.com>
2026-09-23 21:26:30 +0200

    MINI: expose integrated online pairing/runtime hooks

--- firmware/waveshare-terminal/main/mel_terminal.h
+++ firmware/waveshare-terminal/main/mel_terminal.h
@@ -4,9 +4,14 @@
 
 #define MEL_LCD_H_RES 320
 #define MEL_LCD_V_RES 480
-#define MEL_FW_VERSION "0.2.0-mini-test"
+#define MEL_FW_VERSION "0.4.0-mini-integrated"
 #define MEL_PROTOCOL_VERSION "1.0"
 
 void mel_terminal_ui_init(lv_disp_t *display);
 void mel_terminal_start(bool force_setup);
 void mel_terminal_set_hardware(bool camera_ok, bool audio_ok, bool sd_ok);
+
+bool mel_terminal_has_token(void);
+void mel_terminal_set_pair_code(const char *code);
+void mel_terminal_set_network_info(const char *ip);
+void mel_terminal_start_online(void);
