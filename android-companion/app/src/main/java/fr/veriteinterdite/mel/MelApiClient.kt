3f37ecfb076e1dc76d79eec329dc044c272e0225
adrienlopezcarreras-pixel <adrien.lopezcarreras@gmail.com>
2026-09-24 16:17:54 +0200

    Build Android MEL 0.6.10

--- android-companion/app/src/main/java/fr/veriteinterdite/mel/MelApiClient.kt
+++ android-companion/app/src/main/java/fr/veriteinterdite/mel/MelApiClient.kt
@@ -1,4 +1,4 @@
-﻿package fr.veriteinterdite.mel
+package fr.veriteinterdite.mel
 
 import android.util.Base64
 import org.json.JSONArray
@@ -20,7 +20,7 @@ class MelApiClient(
 ) {
     companion object {
         const val PROTOCOL_VERSION = "1.0"
-        const val APP_VERSION = "0.6.9"
+        const val APP_VERSION = "0.6.10"
     }
 
     init {
@@ -243,4 +243,3 @@ class MelApiClient(
         return messages
     }
 }
-
