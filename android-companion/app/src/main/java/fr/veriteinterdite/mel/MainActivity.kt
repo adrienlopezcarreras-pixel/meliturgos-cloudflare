9fe05aa6ef33dc72ada8e133abc559dfdf9e249f
adrienlopezcarreras-pixel <adrien.lopezcarreras@gmail.com>
2026-09-24 10:28:49 +0200

    Android 0.6.8: keep speech Intent while removing browser ACTION_VIEW

--- android-companion/app/src/main/java/fr/veriteinterdite/mel/MainActivity.kt
+++ android-companion/app/src/main/java/fr/veriteinterdite/mel/MainActivity.kt
@@ -4,6 +4,7 @@ import android.Manifest
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.media.MediaRecorder
