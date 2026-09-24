d3373c8e189165809a8016eece9ead715c6c70a1
adrienlopezcarreras-pixel <adrien.lopezcarreras@gmail.com>
2026-09-24 17:38:33 +0200

    Android 0.6.12: bump app version

--- android-companion/app/build.gradle.kts
+++ android-companion/app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "fr.veriteinterdite.mel"
         minSdk = 26
         targetSdk = 35
-        versionCode = 20
-        versionName = "0.6.11"
+        versionCode = 21
+        versionName = "0.6.12"
         buildConfigField("String", "MEL_BASE_URL", "\"https://meliturgos.adrien-lopezcarreras.workers.dev\"")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
