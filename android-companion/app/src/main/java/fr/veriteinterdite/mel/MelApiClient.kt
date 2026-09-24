abb3e1e8bbef879234fbbb1c5d774d96e79a8758
adrienlopezcarreras-pixel <adrien.lopezcarreras@gmail.com>
2026-09-24 16:04:16 +0200

    Android 0.6.10: bump protocol app version

--- android-companion/app/src/main/java/fr/veriteinterdite/mel/MelApiClient.kt
+++ android-companion/app/src/main/java/fr/veriteinterdite/mel/MelApiClient.kt
@@ -20,7 +20,7 @@ class MelApiClient(
 ) {
     companion object {
         const val PROTOCOL_VERSION = "1.0"
-        const val APP_VERSION = "0.6.9"
+        const val APP_VERSION = "0.6.10"
     }
 
     init {
