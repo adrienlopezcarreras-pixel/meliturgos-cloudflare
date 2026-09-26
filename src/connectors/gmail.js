2919e5b6fb47d0a650ab57259742aead7bc8a407
adrienlopezcarreras-pixel <adrien.lopezcarreras@gmail.com>
2026-09-26 21:35:43 +0200

    feat(connectors): declare guarded Gmail read and write actions

--- src/connectors/gmail.js
+++ src/connectors/gmail.js
@@ -1,15 +1,20 @@
 import { Connector } from './sdk.js';
 export const definition = Object.freeze({
-  "id": "gmail",
-  "provider": "google",
-  "auth_type": "OAUTH2",
-  "probe": "https://gmail.googleapis.com/gmail/v1/users/me/profile",
-  "secret_references": [
-    "GMAIL_ACCESS_TOKEN"
+  id: 'gmail',
+  provider: 'google',
+  auth_type: 'OAUTH2',
+  probe: 'https://gmail.googleapis.com/gmail/v1/users/me/profile',
+  secret_references: ['GMAIL_ACCESS_TOKEN'],
+  capabilities: [
+    'gmail.messages.search',
+    'gmail.messages.read',
+    'gmail.drafts.create',
+    'gmail.messages.send'
   ],
-  "capabilities": [
-    "gmail.messages.read"
-  ]
+  scopes: {
+    read: ['https://www.googleapis.com/auth/gmail.readonly'],
+    write: ['https://www.googleapis.com/auth/gmail.compose'],
+    send: ['https://www.googleapis.com/auth/gmail.send']
+  }
 });
-// TODO implement bounded read adapter via CapabilityBus; OAuth grant is external.
 export const createConnector = options => new Connector(definition,options);
